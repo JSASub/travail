4bc26369a936a4c6e20eb60e253ea5a8bc16cd40
JSASub <raoul.aguirre64@gmail.com>
2025-08-17 07:25:33 +0200

    Update main-complete.js

--- main-complete.js
+++ main-complete.js
@@ -679,7 +679,7 @@ function exportToPDF() {
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
           doc.setFontSize(8);
-          doc.text('Correction:', margin + 85, yPosition);
+          doc.text('Correction: ', margin + 85, yPosition);
           doc.setDrawColor(180, 180, 180);
           doc.setLineWidth(0.3);
           doc.line(margin + 105, yPosition + 1, margin + 140, yPosition + 1);
@@ -701,7 +701,7 @@ function exportToPDF() {
         doc.setFont(undefined, 'bold');
         
         // Profondeur prévue
-        doc.text('Prof. prévue:', margin + 5, yPosition);
+        doc.text('Prof. prévue: ', margin + 5, yPosition);
         if (pal.profondeurPrevue && pal.profondeurPrevue.trim()) {
           doc.setTextColor(colors.darkR, colors.darkG, colors.darkB);
           doc.setFont(undefined, 'normal');
@@ -709,30 +709,30 @@ function exportToPDF() {
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
           doc.setFontSize(8);
-          doc.text('Corr:', margin + 35, yPosition);
+          doc.text('Corr: ', margin + 35, yPosition);
           doc.line(margin + 43, yPosition + 1, margin + 53, yPosition + 1);
         } else {
           doc.line(margin + 25, yPosition + 1, margin + 45, yPosition + 1);
-          doc.text('m', margin + 47, yPosition);
+          doc.text(' m', margin + 47, yPosition);
         }
         
         // Durée prévue
         doc.setTextColor(colors.primaryR, colors.primaryG, colors.primaryB);
         doc.setFontSize(9);
         doc.setFont(undefined, 'bold');
-        doc.text('Durée prévue:', margin + 60, yPosition);
+        doc.text('Durée prévue: ', margin + 60, yPosition);
         if (pal.dureePrevue && pal.dureePrevue.trim()) {
           doc.setTextColor(colors.darkR, colors.darkG, colors.darkB);
           doc.setFont(undefined, 'normal');
-          doc.text(pal.dureePrevue + 'min', margin + 82, yPosition);
+          doc.text(pal.dureePrevue + ' min', margin + 82, yPosition);
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
           doc.setFontSize(8);
-          doc.text('Corr:', margin + 95, yPosition);
+          doc.text('Corr: ', margin + 95, yPosition);
           doc.line(margin + 103, yPosition + 1, margin + 113, yPosition + 1);
         } else {
           doc.line(margin + 82, yPosition + 1, margin + 102, yPosition + 1);
-          doc.text('min', margin + 104, yPosition);
+          doc.text(' min', margin + 104, yPosition);
         }
         yPosition += 8;
         
@@ -742,42 +742,42 @@ function exportToPDF() {
         doc.setFont(undefined, 'bold');
         
         // Profondeur réalisée
-        doc.text('Prof. réalisée:', margin + 5, yPosition);
+        doc.text('Prof. réalisée: ', margin + 5, yPosition);
         if (pal.profondeurRealisee && pal.profondeurRealisee.trim()) {
           doc.setTextColor(colors.darkR, colors.darkG, colors.darkB);
           doc.setFont(undefined, 'normal');
-          doc.text(pal.profondeurRealisee + 'm', margin + 28, yPosition);
+          doc.text(pal.profondeurRealisee + ' m', margin + 28, yPosition);
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
           doc.setFontSize(8);
-          doc.text('Corr:', margin + 38, yPosition);
+          doc.text('Corr: ', margin + 38, yPosition);
           doc.setDrawColor(180, 180, 180);
           doc.line(margin + 46, yPosition + 1, margin + 56, yPosition + 1);
         } else {
           doc.setDrawColor(180, 180, 180);
           doc.line(margin + 28, yPosition + 1, margin + 48, yPosition + 1);
-          doc.text('m', margin + 50, yPosition);
+          doc.text(' m', margin + 50, yPosition);
         }
         
         // Durée réalisée
         doc.setTextColor(colors.successR, colors.successG, colors.successB);
         doc.setFontSize(9);
         doc.setFont(undefined, 'bold');
-        doc.text('Durée réalisée:', margin + 60, yPosition);
+        doc.text('Durée réalisée: ', margin + 60, yPosition);
         if (pal.dureeRealisee && pal.dureeRealisee.trim()) {
           doc.setTextColor(colors.darkR, colors.darkG, colors.darkB);
           doc.setFont(undefined, 'normal');
-          doc.text(pal.dureeRealisee + 'min', margin + 85, yPosition);
+          doc.text(pal.dureeRealisee + ' min', margin + 85, yPosition);
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
           doc.setFontSize(8);
-          doc.text('Corr:', margin + 98, yPosition);
+          doc.text('Corr: ', margin + 98, yPosition);
           doc.setDrawColor(180, 180, 180);
           doc.line(margin + 106, yPosition + 1, margin + 116, yPosition + 1);
         } else {
           doc.setDrawColor(180, 180, 180);
           doc.line(margin + 85, yPosition + 1, margin + 105, yPosition + 1);
-          doc.text('min', margin + 107, yPosition);
+          doc.text(' min', margin + 107, yPosition);
         }
         yPosition += 8;
         
@@ -798,7 +798,7 @@ function exportToPDF() {
           // Zone de correction en dessous
           doc.setFont(undefined, 'bold');
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
-          doc.text('Correction paliers:', margin + 5, yPosition);
+          doc.text('Correction paliers: ', margin + 5, yPosition);
           doc.setDrawColor(180, 180, 180);
           doc.line(margin + 35, yPosition + 1, margin + 120, yPosition + 1);
         } else {
@@ -808,7 +808,7 @@ function exportToPDF() {
           doc.setFont(undefined, 'normal');
           doc.setFontSize(8);
           doc.setTextColor(colors.grayR, colors.grayG, colors.grayB);
-          doc.text('(ex: 3min à 3m)', margin + 122, yPosition);
+          doc.text('(ex: 3 min à 3 m)', margin + 122, yPosition);
         }
         yPosition += 10; // Plus d'espace après les paliers
         
