e59fe496c3d17d0d6800106427c1b4b21a7b0e76
JSASub <raoul.aguirre64@gmail.com>
2025-08-23 08:15:00 +0200

    Update main-core.js

--- main-core.js
+++ main-core.js
@@ -153,7 +153,6 @@ async function loadFromFirebase() {
     handleError(error, "Chargement Firebase");
   }
   };
-}
 
 // ===== FONCTIONS UTILITAIRES SÉCURISÉES =====
 function showAuthError(message) {
