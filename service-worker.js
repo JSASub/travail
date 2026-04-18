53f41b3a6521eac897afde6430e0ba2158237790
JSASub <raoul.aguirre64@gmail.com>
2026-04-18 23:48:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 // ===== SERVICE WORKER - JSA PALANQUÉES =====
-// Version : 2.2.1
-// Date : 2025-11-07
+// Version : 2.2.2
+// Date : 2026-04-18
 // Auteur : RA - JSA Subaquatique
 
 // Configuration de la version et du cache
-const APP_VERSION = '2.2.1';
+const APP_VERSION = '2.2.2';
 const CACHE_VERSION = `jsas-v${APP_VERSION}`;
 const CACHE_NAME = `jsas-palanquees-${CACHE_VERSION}`;
 
